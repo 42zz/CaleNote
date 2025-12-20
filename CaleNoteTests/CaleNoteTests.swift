bf128409d2796ba8dfbfafe02da8ad4880acba37
Masaya Kawai <mas@42zz.net>
2025-12-20 16:09:58 +0900

    Initial Commit

---
+++ CaleNoteTests/CaleNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  CaleNoteTests.swift
+//  CaleNoteTests
+//
+//  Created by Masaya Kawai on 2025/12/20.
+//
+
+import Testing
+@testable import CaleNote
+
+struct CaleNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
