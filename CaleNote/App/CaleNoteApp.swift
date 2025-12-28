171fc95cb4cfed3531d456d3342d4679d9aa9918
Masaya Kawai <mas@42zz.net>
2025-12-28 18:45:32 +0900

    reset app

--- CaleNote/App/CaleNoteApp.swift
+++ CaleNote/App/CaleNoteApp.swift
@@ -1,23 +1,10 @@
-import GoogleSignIn
-import SwiftData
 import SwiftUI
 
 @main
 struct CaleNoteApp: App {
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .onOpenURL { url in
-                    GIDSignIn.sharedInstance.handle(url)
-                }
+            ContentView()
         }
-        .modelContainer(for: [
-            JournalEntry.self,
-            CachedCalendarEvent.self,   // 短期
-            ArchivedCalendarEvent.self, // 長期
-            CachedCalendar.self,
-            SyncLog.self                // 開発者向けログ
-        ])
-
     }
 }
